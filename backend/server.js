1a837149dcf40c4a7113cde9d825cada1039c1c0
Michael Barajas <MBarajas@comptia.global>
2024-02-05 17:44:04 +0000

    Adding updated backend code

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
-const fastify = require("fastify")({
-  logger: true,
-});
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
+const fastify = require("fastify")({ logger: true });
+const uuid = require("uuid");
+const powerOutages = [
+  {
+    id: "5a95f050-c560-4fbc-8d91-76b9f7016345", // generated with uuid.v4()
+    address: "123 easy street",
+    startDate: new Date(),
+    active: true,
+  },
+];
+
+fastify.get("/outages", async (request, reply) => {
+  return powerOutages;
 });
 
-fastify.listen({ port: 8989 }, function (err, address) {
-  if (err) {
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8989, host: "0.0.0.0" });
+    console.log(`Server is running at ${fastify.server.address().port}`);
+  } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-  // Server is now listening on ${address}
-});
+};
+start();
